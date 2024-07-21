85b789e6272effa311a9e99266381cd6c3af9f49
eunseo <eunseo>
2024-07-21 10:26:35 +0900

    은서 대여/반납 엔티티 수정

--- src/main/java/com/beyond/kkwoborrow/rental/entity/Rental.java
+++ src/main/java/com/beyond/kkwoborrow/rental/entity/Rental.java
@@ -1,17 +1,22 @@
-package com.beyond.kkwoborrow.rental.model.vo;
+package com.beyond.kkwoborrow.rental.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Rental {
     @Id
     @Column(name = "TransactionID")
-    private int transactionID;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long transactionID;
 
     @NonNull
     @Temporal(TemporalType.TIMESTAMP)
