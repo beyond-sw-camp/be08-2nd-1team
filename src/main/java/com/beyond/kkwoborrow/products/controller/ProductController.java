2eefe64c607439dfd1061eecc569df0b36d06adf
mjms0214 <mjms0214@gmail.com>
2024-07-27 21:19:09 +0900

    feat: add swagger annotation(users, products)

--- src/main/java/com/beyond/kkwoborrow/products/controller/ProductController.java
+++ src/main/java/com/beyond/kkwoborrow/products/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.beyond.kkwoborrow.products.controller;
 import com.beyond.kkwoborrow.products.dto.ProductRequestDto;
 import com.beyond.kkwoborrow.products.dto.ProductResponseDto;
 import com.beyond.kkwoborrow.products.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,8 +17,9 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    // 재품 등록
+    // 제품 등록
     @PostMapping("/products")
+    @Operation(summary = "제품 저장", description = "제품을 저장한다.")
     public ResponseEntity<ProductResponseDto> enroll(@RequestBody ProductRequestDto newProduct){
         ProductResponseDto productResponseDto = productService.save(newProduct);
         if (productResponseDto != null){
@@ -28,7 +31,8 @@ public class ProductController {
 
     // 제품 정보 불러오기
     @GetMapping("/products/{product-id}")
-    public ResponseEntity<ProductResponseDto> getProduct(@PathVariable("product-id") Long productId){
+    @Operation(summary = "특정 제품 조회", description = "특정 제품의 정보를 조회한다.")
+    public ResponseEntity<ProductResponseDto> getProduct(@Parameter(description = "제품 번호", example = "1")@PathVariable("product-id") Long productId){
         ProductResponseDto productResponseDto = productService.getProduct(productId);
         if (productResponseDto != null){
             return new ResponseEntity<>(productResponseDto, HttpStatus.OK);
@@ -42,7 +46,8 @@ public class ProductController {
 
     // 제품 정보 수정
     @PatchMapping("/products/{product-id}")
-    public ResponseEntity<ProductResponseDto> updateProduct(@PathVariable("product-id") Long productId, @RequestBody ProductRequestDto updateProduct){
+    @Operation(summary = "제품 정보 수정", description = "특정 제품의 정보를 수정한다.")
+    public ResponseEntity<ProductResponseDto> updateProduct(@Parameter(description = "제품 번호", example = "1")@PathVariable("product-id") Long productId, @RequestBody ProductRequestDto updateProduct){
         ProductResponseDto productResponseDto = productService.updateProduct(productId, updateProduct);
         if (productResponseDto != null){
             return new ResponseEntity<>(productResponseDto, HttpStatus.OK);
@@ -53,7 +58,8 @@ public class ProductController {
 
     // 제품 정보 삭제
     @DeleteMapping("/products/{product-id}")
-    public void deleteUser(@PathVariable("product-id") Long productId) {
+    @Operation(summary = "제품 정보 삭제", description = "특정 제품의 정보를 삭제한다.")
+    public void deleteUser(@Parameter(description = "제품 번호", example = "1")@PathVariable("product-id") Long productId) {
         productService.deleteProduct(productId);
     }
 
