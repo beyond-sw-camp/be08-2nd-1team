0cd1ed9ce9825e27b85f6c15e67f8d1d285beb12
tkckdnjs <wupby1@gmail.com>
2024-07-29 15:11:48 +0900

    Rental code refactor fix

--- src/main/java/com/beyond/kkwoborrow/rental/controller/RentalController.java
+++ src/main/java/com/beyond/kkwoborrow/rental/controller/RentalController.java
@@ -1,6 +1,5 @@
 package com.beyond.kkwoborrow.rental.controller;
 
-import com.beyond.kkwoborrow.rental.dto.RentalRequestDto;
 import com.beyond.kkwoborrow.rental.dto.RentalResponseDto;
 import com.beyond.kkwoborrow.rental.service.RentalService;
 
@@ -20,21 +19,19 @@ public class RentalController {
     @Autowired
     private RentalService rentalService;
 
-    // 대여 신청 가능 물품 불러오기
-    @PostMapping("/{rent}")
+    @PostMapping("/{postId}")
     @Operation(summary = "대여 신청", description = "대여 가능한 물품의 목록을 조회한다.")
     public ResponseEntity<RentalResponseDto> Rent(
-            @RequestBody RentalRequestDto rentalRequestDto) {
-
-        RentalResponseDto rentalResponseDto = rentalService.Rent(rentalRequestDto);
+            @PathVariable("postId") Long postId) {
+        RentalResponseDto rentalResponseDto = rentalService.Rent(postId);
 
         if (rentalResponseDto != null && rentalResponseDto.getPostId() != null) {
             return new ResponseEntity<>(rentalResponseDto, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-
     }
+
 
     @PostMapping("/return/{transactionID}")
     @Operation(summary = "반납 신청", description = "반납해야할 물품 목록을 조회한다.")
--- src/main/java/com/beyond/kkwoborrow/rental/service/RentalServiceImpl.java
+++ src/main/java/com/beyond/kkwoborrow/rental/service/RentalServiceImpl.java
@@ -2,7 +2,6 @@ package com.beyond.kkwoborrow.rental.service;
 
 import com.beyond.kkwoborrow.post.entity.Posts;
 import com.beyond.kkwoborrow.post.repository.PostRepository;
-import com.beyond.kkwoborrow.rental.dto.RentalRequestDto;
 import com.beyond.kkwoborrow.rental.dto.RentalResponseDto;
 import com.beyond.kkwoborrow.rental.entity.Rental;
 import com.beyond.kkwoborrow.rental.repository.RentalRepository;
@@ -20,17 +19,16 @@ public class RentalServiceImpl implements RentalService {
     private PostRepository postRepository;
 
     @Override
-    public RentalResponseDto Rent(RentalRequestDto rent) {
-        Posts post = postRepository.findById(rent.getPostId()).orElseThrow(() -> new IllegalArgumentException("Invalid PostID"));
+    public RentalResponseDto Rent(Long postId) {
+        Posts post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("Invalid PostID"));
 
-        Optional<Rental> rental = rentalRepository.findById(rent.getPostId());
+        Optional<Rental> rental = rentalRepository.findById(postId);
 
         return rental.map(RentalResponseDto::new).orElse(null);
     }
 
     @Override
     public RentalResponseDto Return(Long transactionID) {
-        // rentalRepository 에서 transactionID를 찾아 그리고 만약에 없으면 에러 띄우기
         Rental rental = rentalRepository.findById(transactionID).orElseThrow(() -> new IllegalArgumentException("transactionID Not found"));
 
         return new RentalResponseDto(rental);
