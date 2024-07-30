12905beef9d719cb35884c03e682ba585355bcb8
DESKTOP-QPBSKD8\Playdata <ahning1203@gmail.com>
2024-07-30 14:09:30 +0900

    feat: login

--- src/main/java/com/beyond/kkwoborrow/users/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.beyond.kkwoborrow.users.repository;
-
-import com.beyond.kkwoborrow.users.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-    Users findByUserName(String username);
-
-}
