9a6aa57631b4156cbc4d1507daa328848731766e
ideapple <56536737+ideapple@users.noreply.github.com>
2024-07-31 09:37:00 +0900

    Update ReservationRequestDto.java

--- src/main/java/com/beyond/kkwoborrow/reservation/dto/ReservationRequestDto.java
+++ src/main/java/com/beyond/kkwoborrow/reservation/dto/ReservationRequestDto.java
@@ -6,5 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ReservationRequestDto {
+    @Schema(description = "post_id", example = "1")
     private Long postId; // 예약에 필요한 정보가 더 있으면 여기에 추가할 수 있습니다.
 }
